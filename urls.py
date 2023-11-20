feebfc7520117af50e598137750cffe2f88323b8
Monaem Tarek <11410120210220@stud.cu.edu.eg>
2023-11-20 15:53:46 +0200

    change send_response_url port num to 5001

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ GET_MSG_ROUTE = '/api/get-msg'
 API_PORT = 5000  # flask api port
 
 # url to send status upon completion of sending message
-SEND_RESPONSE_URL = 'http://192.155.107.231:5003/receive/statusircs'
+SEND_RESPONSE_URL = 'http://192.155.107.231:5001/receive/statusircs'
 
